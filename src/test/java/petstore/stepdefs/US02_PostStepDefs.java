a9914c2dbb03fd7fc04c2ed09982ba1c852dd659
gumburtu <aslanseval1986@gmail.com>
2025-08-25 12:27:14 +0100

    Post54

--- src/test/java/petstore/stepdefs/US02_PostStepDefs.java
+++ src/test/java/petstore/stepdefs/US02_PostStepDefs.java
@@ -122,8 +122,6 @@ public class US02_PostStepDefs {
 
 
     // Create dummy image content (simplified - you might want to use actual image bytes
-
-
     private byte[] createDummyImageContent() {
         // This is a minimal JPEG header - for testing purposes
         // In a real scenario, you'd use a proper test image file
