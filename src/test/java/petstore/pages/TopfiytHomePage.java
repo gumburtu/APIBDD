ee3db48e037d7510c6858c7f2e90b9b8901e949f
gumburtu <aslanseval1986@gmail.com>
2025-10-21 00:01:58 +0100

    Post117

--- src/test/java/petstore/pages/TopfiytHomePage.java
+++ src/test/java/petstore/pages/TopfiytHomePage.java
@@ -37,6 +37,8 @@ public class TopfiytHomePage {
     }
 
     // Scroll pozisyonunu alma
+
+
     public long getCurrentScrollPosition() {
         return (Long) js.executeScript("return window.pageYOffset;");
     }
